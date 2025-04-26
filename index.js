962458068b32efb7e20888ad99479945117d111b
Nick-hunterr <nickhunter379@gmail.com>
2025-04-26 19:57:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+let server = require('./pair'),
+code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/pair', server);
 app.use('/code', code);
 app.use('/pair',async (req, res, next) => {
 res.sendFile(__path + '/pair.html')
