d9dca31964eba060d3d0b14a03556488bb2719c8
Nick-hunterr <nickhunter379@gmail.com>
2025-04-26 19:19:40 +0300

    Update and rename keithkeizzah.js to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 let server = require('./qr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
@@ -16,11 +16,8 @@ res.sendFile(__path + '/main.html')
 })
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
-
- Server running on http://localhost:` + PORT)
+app.listen(port, () => {
+    console.log(`📡 Connected on http://localhost:` + port)
 })
 
 module.exports = app
