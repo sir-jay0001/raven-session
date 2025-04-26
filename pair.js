fa82c21713930a095b481dae363376f881f18b6a
Nick-hunterr <nickhunter379@gmail.com>
2025-04-26 19:04:40 +0300

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,87 @@
+const PastebinAPI = require('pastebin-js');
+const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { makeid } = require('./id');
+const express = require('express');
+const fs = require('fs');
+const pino = require('pino');
+const {
+    default: Brasho_Kish,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+} = require('maher-zubair-baileys');
+
+const router = express.Router();
+
+// Helper function to remove files
+function removeFile(filePath) {
+    if (!fs.existsSync(filePath)) return false;
+    fs.rmSync(filePath, { recursive: true, force: true });
+}
+
+// Route handler
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+
+    async function LEGACY_MD_PAIR_CODE() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            const Pair_Code_By_Brasho_Kish = Brasho_Kish({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
+                browser: ['Chrome (Linux)', '', '']
+            });
+
+            if (!Pair_Code_By_Brasho_Kish.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Pair_Code_By_Brasho_Kish.requestPairingCode(num);
+
+                 if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            Pair_Code_By_Brasho_Kish.ev.on('creds.update', saveCreds);
+            Pair_Code_By_Brasho_Kish.ev.on('connection.update', async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection === 'open') {
+                await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, { text: `Sending Session_id now... Wait a moment ` });
+                    await delay(50000);
+                    
+                    const data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    await delay(8000);
+                    const b64data = Buffer.from(data).toString('base64');
+                    const session = await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, { text: '' + b64data });
+
+                    // Send message after session
+                    await Pair_Code_By_Brasho_Kish.sendMessage(Pair_Code_By_Brasho_Kish.user.id, {text: `Raven has been linked to your WhatsApp account! Do not share this session_id with anyone. 
+
+Copy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nhttps://wa.me/message/YNDA2RFTE35LB1\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉. ` }, { quoted: session });
+                    
+                    await delay(100);
+                    await Pair_Code_By_Brasho_Kish.ws.close();
+                    removeFile('./temp/' + id);
+                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
+                    await delay(10000);
+                    LEGACY_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log('service restarted', err);
+            removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: 'Service Currently Unavailable' });
+            }
+        }
+    }
+
+    await LEGACY_MD_PAIR_CODE();
+});
+
+module.exports = router;
