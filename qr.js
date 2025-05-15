a126e79b94e72e172923f6aef70d9bddf1751daa
Nick-hunterr <nickhunter379@gmail.com>
2025-05-15 13:21:05 +0300

    Update qr.js

--- qr.js
+++ qr.js
@@ -59,9 +59,9 @@ router.get('/', async (req, res) => {
 				   let b64data = Buffer.from(data).toString('base64');
 				   let session = await client.sendMessage(client.user.id, { text: '' + b64data });
 	
-let Textt = `Raven has been linked to your WhatsApp account! Do not share this session_id with anyone.\nCopy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nhttps://wa.me/message/YNDA2RFTE35LB1\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉. `
+let Textt = `Raven has been linked to your WhatsApp account! Do not share this session_id with anyone.\nCopy and paste it on the SESSION string during deploy as it will be used for authentication.\n\nIncase you are facing Any issue reach me via here👇\n\nhttps://wa.me/message/YNDA2RFTE35LB1\n\nAnd don't forget to sleep😴, for even the rentless must recharge⚡.\n\nGoodluck 🎉.`
 	
-			await client.sendMessage(client.user.id,{text:Textt},{quoted:session})
+			await client.sendMessage(client.user.id,{ text: Textt }, {quoted: session })
 
 
 					await delay(100);
